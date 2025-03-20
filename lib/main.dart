836d48b76dc9dfc82d8ea6c47d53d5784085f442
bimbim1214 <bimoadityapangestu@gmail.com>
2025-03-20 19:49:58 +0700

    Menambahkan halaman utama dengan StatefulWidget.

--- lib/main.dart
+++ lib/main.dart
@@ -20,3 +20,13 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => MyHomePageState();
+}
+
+
